41e0d4969b92b90681f81b284ba910a14bcb5c13
sanket bodke <sanketbodke26@gmail.com>
2023-08-12 22:26:43 +0530

    search add

--- pages/News.jsx
+++ pages/News.jsx
@@ -1,63 +1,133 @@
-import React, {useEffect} from 'react';
-import {StyleSheet, Text, View, Image, Linking, Button} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Linking,
+  TextInput,
+  Button,
+} from 'react-native';
 import axios from 'axios';
 import {useQuery} from '@tanstack/react-query';
 
 export default function News() {
-  const {data, refetch} = useQuery(
-    ['news'],
-    () =>
-      axios
-        .get(
-          'https://newsapi.org/v2/top-headlines?category=general&country=in&apiKey=0a896ee86f6b404b8a695b5e43449f30',
-        )
-        .then(resp => resp.data),
+  const {data, refetch} = useQuery(['news'], () =>
+    axios
+      .get(
+        `https://newsapi.org/v2/top-headlines?category=${category}&country=in&apiKey=0a896ee86f6b404b8a695b5e43449f30`,
+      )
+      .then(resp => resp.data),
   );
 
   useEffect(() => {
     refetch();
   }, []);
 
-  const openNews = (url)=>{
-    Linking.openURL(url)
-    .catch((error) => console.error('An error occurred: ', error));
-  }
+  const openNews = url => {
+    Linking.openURL(url).catch(error =>
+      console.error('An error occurred: ', error),
+    );
+  };
+
+  const currentDate = new Date();
+  const day = currentDate.getDay();
+  const date = currentDate.getDate();
+  const month = currentDate.getMonth();
+
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  const dayNames = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  const currentDayName = dayNames[day];
+
+  const monthName = monthNames[month];
+
+  const [category, setCategory] = useState('general');
+
+  const articles = data?.articles || [];
+
+  const inputAccessoryViewID = 'uniqueID';
 
   return (
     <>
-      <View>
-        <Text>Time Heading etc....</Text>
+      <View style={styles.titleContainer}>
+        <Text>Notizia</Text>
+        <Text>{`${currentDayName}, ${date} ${monthName}`}</Text>
       </View>
-      <View style={styles.newsContainer}>
-        {data.articles &&
-          data.articles.map((news, index) => (
-            <>
-              <View style={styles.newsContent}>
-                <View style={styles.newsImageContainer}>
-                  <Image
-                    style={styles.newsImage}
-                    source={{
-                      uri: news.urlToImage,
-                    }}
-                  />
-                </View>
-                <View style={styles.newDataContainer}>
-                  <Text style={styles.newsPublish}>{news.publishedAt}</Text>
-                  <Text style={styles.newsTitle}>{news.title}</Text>
-                  <Text style={styles.newsDescription}>
-                    {news.content && news.content.slice(0, 200)}
-                  </Text>
-                  <Text style={styles.readMoreBtn} onPress={()=> openNews(news.url)}>Read More</Text>
-                </View>
+      <View style={styles.searchBox}>
+        <TextInput
+          style={styles.searchInput}
+          onChangeText={setCategory}
+          placeholder={'Enter Category'}
+          inputAccessoryViewID={inputAccessoryViewID}
+        />
+
+        <Text style={styles.searchBtn} onPress={refetch}>Search</Text>
+      </View>
+      <View>
+        {articles.map((news, key) => (
+          <>
+            <View style={styles.newsContent}>
+              <View style={styles.newsImageContainer}>
+                <Image
+                  key={key}
+                  style={styles.newsImage}
+                  source={{
+                    uri: news.urlToImage,
+                  }}
+                />
               </View>
-            </>
-          ))}
+              <View style={styles.newDataContainer}>
+                <Text style={styles.channelName}>{news.source.name}</Text>
+                <Text style={styles.newsPublish}>{news.publishedAt}</Text>
+                <Text style={styles.newsTitle}>{news.title}</Text>
+                <Text style={styles.newsDescription}>
+                  - {news.content && news.content.slice(0, 200)}
+                </Text>
+                <Text
+                  style={styles.readMoreBtn}
+                  onPress={() => openNews(news.url)}>
+                  Read More
+                </Text>
+              </View>
+            </View>
+          </>
+        ))}
       </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  titleContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 15,
+    marginVertical: 17,
+  },
   newsContent: {
     flex: 1,
     justifyContent: 'center',
@@ -67,9 +137,35 @@ const styles = StyleSheet.create({
     marginBottom: 40,
   },
 
+  searchBox: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 15,
+    marginBottom: 20,
+    marginTop: 20,
+  },
+
+  searchInput: {
+    padding: 15,
+    backgroundColor: '#ffffff',
+    color: '#000000',
+    padding: 10,
+    fontSize: 12,
+    width: '80%',
+  },
+
+  searchBtn: {
+    backgroundColor: '#000000',
+    color: '#ffffff',
+    padding: 10,
+    fontSize: 12,
+  },
+
   newsImage: {
     width: 400,
-    height: 190,
+    height: 220,
     objectFit: 'cover',
     borderRadius: 3,
   },
@@ -78,24 +174,35 @@ const styles = StyleSheet.create({
     width: 400,
   },
 
+  channelName: {
+    marginVertical: 9,
+    fontSize: 16,
+    fontWeight: '700',
+  },
+
+  newsPublish: {
+    fontSize: 16,
+    fontWeight: '700',
+  },
+
   newsTitle: {
     fontSize: 20,
     letterSpacing: 1,
     fontWeight: 'bold',
-    marginVertical: 9
+    marginVertical: 9,
   },
 
   newsDescription: {
     fontSize: 18,
-    letterSpacing: 1
+    letterSpacing: 1,
   },
 
-  readMoreBtn:{
+  readMoreBtn: {
     color: '#ffffff',
     marginVertical: 15,
-    backgroundColor: '#000003',
+    backgroundColor: '#000000',
     textAlign: 'center',
     padding: 12,
     fontWeight: '600',
-  }
+  },
 });
